96348e058d82a66180e7642232ae83d6f26bb23b
kgarousi <kgarousi32@gmail.com>
2024-08-25 16:12:50 -0500

    add error message for divide by zero

--- script.js
+++ script.js
@@ -25,7 +25,7 @@ function multiply(num1, num2){
 function divide(num1, num2){
     Number(num2)
     if (num2 == 0){
-        displayValue("Cannot divide by zero or undefined")
+        setTimeout(() => displayValue("Cannot divide by zero or undefined"), 500)
     }else{
         return Number(num1) / Number(num2);    
     }
